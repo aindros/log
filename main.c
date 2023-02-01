2326ed83da161a4bd0f15ad1897a04d8a640c34a
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-02-01 19:28:33 +0100

    Add a test-app to test the library

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "log.h"
+
 int
 main(int argc, char **argv)
 {
+	Log *log = log_create(__FILE__);
+	log_debug(log, "test log ");
+
 	return 0;
 }
