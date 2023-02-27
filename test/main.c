f22b551ec4232bc1ac4ec089fa60fe97c78d0b3c
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-02-27 17:44:51 +0100

    Add a printf to check when tests start

--- test/main.c
+++ test/main.c
@@ -1,10 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <stdio.h>
 #include "log.h"
 
 int
 main(int argc, char **argv)
 {
+	printf("Init tests\n\n");
+
 	Log *log = log_create(__FILE__);
 	log_debug(log, "debug log");
 	log_info(log, "info log");
