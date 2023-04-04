ae06c0483f3e48ff664da4678e519e9a69cd453b
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2023-04-04 23:53:08 +0200

    Move variables in config.mk

---
+++ config.mk
@@ -0,0 +1,10 @@
+# See LICENSE file for copyright and license details.
+
+CC   = cc
+SRC != find src -name "*.c"
+OBJ  = ${SRC:.c=.o}
+
+LIBNAME = liblog
+LIBVER  = 0.0.0-a1
+
+CFLAGS  = -Wall -ansi --std=c89 -pedantic ${OPT} -DLIBVER=\"${LIBVER}\"
